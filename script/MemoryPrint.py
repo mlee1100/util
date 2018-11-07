8cf5ef001321042ce14888af6852a423265e8669
Michael Lee <mlee1100@gmail.com>
2018-11-07 14:57:43 +0000

    memory print tracking max of session

--- script/MemoryPrint.py
+++ script/MemoryPrint.py
@@ -1,6 +1,16 @@
 import psutil
 import time
 
+max_mem = 0
 while True:
-    print psutil.virtual_memory().percent
-    time.sleep(1)
+    try:
+        mem = psutil.virtual_memory().percent
+        print mem
+        if mem > max_mem:
+            max_mem = mem
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print '\nmax memory of session: {}%'.format(max_mem)
+        break
+    except:
+        raise
